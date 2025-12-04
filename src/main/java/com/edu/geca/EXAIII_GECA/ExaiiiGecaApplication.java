293d084ed50440d0430825baf91e1b99d1ae4d5d
GabrielaCohailaAlvaradoE <gc2022075746@virtual.upt.pe>
2025-12-04 11:02:48 -0500

    sisisisisi

--- src/main/java/com/edu/geca/EXAIII_GECA/ExaiiiGecaApplication.java
+++ src/main/java/com/edu/geca/EXAIII_GECA/ExaiiiGecaApplication.java
@@ -2,12 +2,18 @@ package com.edu.geca.EXAIII_GECA;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ExaiiiGecaApplication {
+public class ExaiiiGecaApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ExaiiiGecaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ExaiiiGecaApplication.class, args);
+    }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ExaiiiGecaApplication.class);
+    }
 }
